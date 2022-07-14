f03e634b8a73ef84a38ffb31c6d1fd44f3478491
star2871 <star2871@gmail.com>
2022-07-14 17:55:53 +0900

    pytho 4일차

--- python/파이썬기초 4일차/실습풀이/17번문제.py
+++ python/파이썬기초 4일차/실습풀이/17번문제.py
@@ -1,5 +1,4 @@
 word = str(input())
-word_change = []
 for word_change in word:
     temp = word_change.replace("b", "B").replace("a", "A").replace("n", "N")
     print(temp)
--- python/파이썬기초 4일차/실습풀이/18번문제.py
+++ python/파이썬기초 4일차/실습풀이/18번문제.py
@@ -4,6 +4,6 @@ for b in word:
     if b in count_words:
         count_words[b] += 1
     else :
-        count_words[b] =1
+        count_words[b] = 1
 print(count_words)
     
