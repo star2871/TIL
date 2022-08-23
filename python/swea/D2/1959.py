24955d942f13ca4c95ec2b8b3e30f3a63426cae7
star2871 <star2871@gmail.com>
2022-08-23 16:59:25 +0900

    db 6일차 실습완료, swea 1959 문제풀이완료

--- python/swea/D2/1959.py
+++ python/swea/D2/1959.py
@@ -5,12 +5,26 @@ sys.stdin = open("1959.txt")
 T = int(input())
 
 for tc in range(1, T+1):
-    n, m = list(map(int, input().split()))
+    n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    if n>= m:
-        for i in range(1, n-m+n+1):
-
-    elif n < m:
-        for i in range(m-n+n, m-n):
     
+    if n == m:
+        max = 0
+        for i in range(n):
+            max +=a[i] * b[i]
+    else:
+        if n > m :
+            sum = [0] * (n-m+1)
+            for i in range(len(sum)):
+                for j in range(-1, -m-1, -1):
+                    sum[i] += a[j-i] * b[j]
+        else:
+            sum = [0] * (m-n+1)
+            for i in range(len(sum)):
+                for j in range(-1, -n-1, -1):
+                    sum[i] += a[j] * b[j-i]
+        sum.sort()
+        max = sum[-1]
+
+    print(f'#{tc} {max}')
