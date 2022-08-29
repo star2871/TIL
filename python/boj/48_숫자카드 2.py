1143edb23aaaa9b4a7a9f0297f3c79d650eace50
star2871 <star2871@gmail.com>
2022-08-29 17:22:10 +0900

    web 1일차 css정리 밑 html정리 및 배운거 복습

--- python/boj/48_숫자카드 2.py
+++ python/boj/48_숫자카드 2.py
@@ -1,3 +1,11 @@
 import sys
 
 sys.stdin = open("48_숫자카드 2.txt")
+
+A = int(input())
+num_card = list(map(int,input().split()))
+B = int(input())
+card_number = list(map(int,input().split()))
+
+for i in range(len(num_card)):
+    print(num_card[i])
