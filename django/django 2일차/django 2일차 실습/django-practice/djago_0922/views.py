58e99270af3f397a9b5643d25e85e2d687458876
star2871 <star2871@gmail.com>
2022-09-23 00:05:06 +0900

    수정

--- django/django 2일차/django 2일차 실습/django-practice/djago_0922/views.py
+++ django/django 2일차/django 2일차 실습/django-practice/djago_0922/views.py
@@ -7,12 +7,20 @@ import numpy as np
 def dinner(request):
 
     foods = ['삼겹살', '치킨', '피자', '파스타', '감자탕']
-
+    images ={
+      '삼겹살': 'https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg',
+      '치킨': 'http://www.bhc.co.kr/upload/bhc/menu/410x271_view_%EC%B9%98%ED%90%81%EB%8B%B9%ED%95%9C%EB%A7%88%EB%A6%AC(0).png',
+      '피자': 'https://w.namu.la/s/8c2aebf04d4c6e0ae24ebf3b3789cb064f353da40f0a2916630ee33cc34742414ac8427b8765569e84d615a24cac7bc389ada2e5c60579541ea8b41be9b22db6d0ce58f59fd1ac01912436c928605cd86974e360258a66ac0374662e70b0ae73',
+      '파스타': 'https://static.wtable.co.kr/image/production/service/recipe/1802/9c556903-01f9-4850-bcf8-1f9c62a0d680.jpg?size=1050x590',
+      '감자탕': 'https://recipe1.ezmember.co.kr/cache/recipe/2018/12/05/ae4988aeb11d9351478f37168b19f4b41.jpg'
+    }
     food = random.choice(foods)
     
+    image = images[food]
 
     context ={
         'food': food,
+        'image':image
     }
     return render(request, 'dinner.html', context)
 
