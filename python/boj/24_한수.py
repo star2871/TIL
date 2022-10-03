af69e364a360348468838e162f846c774ac927e8
star2871 <star2871@gmail.com>
2022-10-03 22:06:09 +0900

    boj 1065번 한수 문제풀이완료

--- python/boj/24_한수.py
+++ python/boj/24_한수.py
@@ -1,3 +1,15 @@
 import sys
 
 sys.stdin = open("24_한수.txt")
+
+n = int(input())
+han = 0
+for i in range(1, n + 1):
+    if i < 100:
+        han += 1
+    else:
+        ns = list(map(int, str(i)))
+        # 일의자리와 십의자리차와 십의자리와 백의자리차같을때
+        if ns[0] - ns[1] == ns[1] - ns[2]:
+            han += 1
+print(han)
