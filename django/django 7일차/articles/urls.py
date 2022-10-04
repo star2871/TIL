590e0a8c61ef9806b212ca6071fd352dd7220764
star2871 <star2871@gmail.com>
2022-10-04 17:03:22 +0900

    10 - ModelForm

--- django/django 7일차/articles/urls.py
+++ django/django 7일차/articles/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # http://127.0.0.1:8000/articles/
     path("", views.index, name="index"),
     # http://127.0.0.1:8000/articles/new/
-    path("new/", views.new, name="new"),
+    # path("new/", views.new, name="new"),
     # http://127.0.0.1:8000/articles/create/
     path("create/", views.create, name="create"),
 ]
--- django/django 7일차/articles/views.py
+++ django/django 7일차/articles/views.py
@@ -2,19 +2,35 @@ from django.shortcuts import render, redirect
 
 from articles.models import Article
 
+from .forms import ArticleForm
+
 # Create your views here.
 def index(request):
     articles = Article.objects.order_by("-pk")
-    context = {"articles": articles}
+    context = {
+        "articles": articles,
+    }
     return render(request, "articles/index.html", context)
 
 
-def new(request):
-    return render(request, "articles/new.html")
+# def new(request):
+#     article_form = ArticleForm()
+#     context = {
+#         "article_form": article_form,
+#     }
+#     return render(request, "articles/new.html", context=context)
 
 
 def create(request):
-    title = request.POST.get("title")
-    content = request.POST.get("content")
-    Article.objects.create(title=title, content=content)
-    return redirect("articles:index")
+    if request.method == "POST":
+        # DB에 저장하는 로직
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("articles:index")
+    else:
+        article_form = ArticleForm()
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "articles/new.html", context=context)
