2cd9cbb86342a4aee804116f1f1305c93d076b64
star2871 <star2871@gmail.com>
2022-10-12 17:48:43 +0900

    django 11일차 완료

---
+++ django/django 11일차/accounts/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
+
+# Create your views here.
+def index(request):
+    return render(request, "accounts/index.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
+
+
+def list(request):
+    user = get_user_model().objects.order_by("-pk")
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/list.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # 세션에 저장
+            # login 함수는 request, user 객체를 인자로 받음
+            # user 객체는 어디있어요? 바로 form에서 인증된 유저 정보를 받을 수 있음
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get("next") or "accounts:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
