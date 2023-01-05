6958f049d1a4aff38a584ee51d1daeebc0fc108a
star2871 <star2871@gmail.com>
2023-01-05 17:22:17 +0900

    drf 글작성 crud 및 목록보여주기

--- drf toyproject/accountapp/urls.py
+++ drf toyproject/accountapp/urls.py
@@ -1,7 +1,17 @@
+# from django.urls import path
+# from accountapp.views import hello_world, hello_world_drf
+# from . import views
+# urlpatterns = [
+#     path('', views.hello_world, name='hello_world'),
+#     path('hello_world_drf', hello_world_drf),
+# ]
 from django.urls import path
-from accountapp.views import hello_world, hello_world_drf
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
-urlpatterns = [
-    path('hello_world', hello_world),
-    path('hello_world_drf', hello_world_drf),
+urlpatterns =[
+    path('blog/', views.BlogList.as_view()),
+    path('blog/<int:pk>/', views.BlogDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
