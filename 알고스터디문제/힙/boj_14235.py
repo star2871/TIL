e9204bfe68ae672509bbaf812b3d81daa37a0c55
star2871 <star2871@gmail.com>
2023-01-19 22:28:08 +0900

    boj_1138 풀이완료

--- 알고스터디문제/힙/boj_14235.py
+++ 알고스터디문제/힙/boj_14235.py
@@ -6,8 +6,8 @@ n = int(input())
 gift = []
 
 for i in range(n):
-    a = list(map(int, input().split()))
-    if a[0] == 0:
+    list_a = list(map(int, input().split()))
+    if list_a[0] == 0:
         if len(gift) == 0:
             print(-1)
         else:
@@ -15,5 +15,5 @@ for i in range(n):
             print(tmp)
     else:
         # 인풋 값에서 앞에 2는 갯수를 의미한다.
-        for j in range(a[0]):
-            heapq.heappush(gift, -a[j + 1])
+        for j in range(list_a[0]):
+            heapq.heappush(gift, -list_a[j + 1])
